82cae85e6008c5cceaf325cf5df66fe8ea94f056
joseehilton <33073326+joseehilton@users.noreply.github.com>
2017-12-14 15:10:46 -0200

    Atualização Decimal

--- calculadora.js
+++ calculadora.js
@@ -9,9 +9,14 @@ jQuery(document).ready(function($) {
         area_teto = jQuery('input[name=area_teto]').val().replace(",", ".");
         constante_tinta = jQuery('select[name=tinta]').val().replace(",", ".");
         resultado = ((parseFloat(area_parede) + parseFloat(area_teto)) / constante_tinta).toFixed(2);
+        obs = 0.1 * resultado;
+        console.log(obs.toFixed(2));
         jQuery('.resultado-tinta').show();
         jQuery('.resultado-tinta').html('');
         jQuery('.resultado-tinta').html('<span class="label label-danger">RESULTADO:</span> Serão necessários <b>' + resultado + '</b> litro(s).');
+        jQuery('.resultado-tinta2').show();
+        jQuery('.resultado-tinta2').html('<span class="label label-danger">ATENÇÃO:</span> Recomendamos que compre <b>' + obs + '</b> litro(s) à mais devido à algum imprevisto tais como: desperdício ou outro fator.');
+        
         return false;
     });
 
@@ -26,6 +31,7 @@ jQuery(document).ready(function($) {
         resultado = (parseFloat(area_superficie) / parseFloat(area_revestimento));
         resultado = (parseFloat(resultado) + (parseFloat(resultado) * 0.1)).toFixed(2);
         obs = 0.1 * resultado;
+        console.log(obs.toFixed(2));
         jQuery('.resultado-piso').show();
         jQuery('.resultado-piso').html('');
         jQuery('.resultado-piso').html('<span class="label label-danger">RESULTADO:</span> Serão necessários <b>' + resultado + '</b> peça(s).');
@@ -42,6 +48,7 @@ jQuery(document).ready(function($) {
         constante_telha = jQuery('select[name=tipo_telha]').val().replace(",", ".");
         resultado = parseFloat(area_cobertura) * parseFloat(constante_telha);
         obs = 0.1 * resultado;
+        console.log(obs.toFixed(2));
         jQuery('.resultado-telha').show();
         jQuery('.resultado-telha').html('');
         jQuery('.resultado-telha').html('<span class="label label-danger">RESULTADO:</span> Serão necessárias <b>' + resultado + '</b> telha(s).');
@@ -58,6 +65,7 @@ jQuery(document).ready(function($) {
         tipo_tijolos = jQuery('select[name=tipo_tijolos]').val().replace(",", ".");
         resultado = area_superficie * tipo_tijolos;
         obs = 0.1 * resultado;
+        console.log(obs.toFixed(2));
         jQuery('.resultado-tijolo').show();
         jQuery('.resultado-tijolo').html('');
         jQuery('.resultado-tijolo').html('<span class="label label-danger">RESULTADO:</span> Serão necessários <b>' + resultado + '</b> tijolo(s).');
